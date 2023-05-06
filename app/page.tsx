ee4693517c6311f45eeaa0026a2fbd97aa277359
sylvain joly <sylvain.joly00@orange.fr>
2023-05-06 02:34:34 +0200

    go

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <div className='mx-auto container mt-20'>
+        <p className='text-6xl'>LégalDealer.fr</p>
+      </div>
+    </main>
+  )
+}
