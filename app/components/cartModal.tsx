10470e27a1a024e5a0690995929e71b997ae0a68
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-05-11 16:38:09 +0200

    ]UPDATE[

--- app/components/cartModal.tsx
+++ app/components/cartModal.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef, useState } from "react"
 export default function CartModal ({isOpen, onClose, cart}: {onOpen: any, isOpen: any, onClose: any, cart:any}) {
     const btnRef = useRef()
 
-
     return ( 
         <div>
             <Drawer 
@@ -18,13 +17,21 @@ export default function CartModal ({isOpen, onClose, cart}: {onOpen: any, isOpen
                     
                     <p className='text-2xl md:text-6xl font-bold border-b-[0.1em] border-emerald-500 ml-4 mt-5'>Panier</p>
 
-                    <DrawerBody>
+                    <DrawerBody className="mt-5 text-2xl">
+
+                        {
+                            cart && Object.keys(cart).length > 0 && Object.keys(cart).map((productKey: string) => (
+                                <div>
+                                    <p>{cart[productKey].name}</p>
+                                    <p> x {cart[productKey].quantity} </p>
+
+                                </div>
+                            ))
+                        }
+
                     </DrawerBody>
 
-                    {
-                        cart && Object.keys(cart).length > 0 && Object.keys(cart).map((productKey: string) => <p>{cart[productKey].name}</p>)
-                    }
-
+  
                     {
                         Object.keys(cart).length === 0 && (
                             <p className='text-center text-2xl md:text-6xl font-bold mt-10'>Votre panier est vide</p>
