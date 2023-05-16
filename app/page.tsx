3a6c9e370c4e414f5861cb44c9cc989665a77250
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-05-17 01:20:57 +0200

    w

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ import { useEffect, useState } from 'react';
 import {LocalStorageManager, addToCart} from './db/localStorageManager';
 import { FiThermometer, FiPackage, FiTag} from 'react-icons/fi';
 import { BiRuler } from 'react-icons/bi';
+import { BsFillStarFill } from "react-icons/bs";
+import Link from 'next/link';
+import { FaTiktok } from 'react-icons/fa';
 
 // export async function getServerSideProps () {
 
@@ -151,27 +154,38 @@ export default function Home() {
   return (
     <main>
 
-  { JSON.stringify(stripeProductsList) }
+    {/* { JSON.stringify(stripeProductsList) }
   
       
-      <p>CURRENT CART : {JSON.stringify(cart)}</p>
-      
+      <p>CURRENT CART : {JSON.stringify(cart)}</p> */}
+{/*       
       <div className='mx-auto container mt-10 flex justify-center items-center'>
         <img src="/weed.png" className='w-[64px]'></img>
         <p className='text-4xl md:text-6xl font-bold border-b-[0.1em] border-emerald-500 ml-4'>Weedy</p>
-      </div>
+      </div> */}
 
-      <div className="bg-black text-white rounded-lg md:shadow-md overflow-hidden mx-auto container mt-10 bg-[url('https://media.discordapp.net/attachments/1102026209472286781/1107783960207040622/bsqkt_une_dalle_de_lave_08eb9515-b724-4d5b-b09a-dbf1b36f4ba4.png?width=974&height=974')] bg-no-repeat bg-cover bg-center bg-fixed">
+      <header className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto flex items-center justify-between">
+            <a className="flex items-center">
+              <img src="/logo.svg" alt="Logo" className="h-6 w-6 mr-2" />
+              <h1 className="text-xl font-bold">Project Title</h1>
+            </a>
+          {/* Add any additional header content, such as navigation links */}
+        </div>
+      </header>
+
+
+      <div className="bg-black text-white md:shadow-md overflow-hidden bg-[url('https://media.discordapp.net/attachments/1102026209472286781/1107783960207040622/bsqkt_une_dalle_de_lave_08eb9515-b724-4d5b-b09a-dbf1b36f4ba4.png?width=974&height=974')] bg-no-repeat bg-cover bg-center bg-fixed">
         <div className={`flex flex-col md:flex-row items-center shadow-lg backdrop-brightness-50`} >
           <div className="w-full md:w-1/2 mb-6 md:mb-0">
             {/* Product Image */}
-            <img src="https://www.expondo.fr/media/image/71/7a/a7/10010882-3_600x600.jpg" alt="Product" className="w-full h-auto" />
+            <img src="https://ae01.alicdn.com/kf/S69c604a0e38b4cd9a1c1ac79ad49e592g/Machine-kebab-lectrique-sans-fum-e-en-acier-inoxydable-6-b-tons-barbecue-vertical-modules-distance.jpg_Q90.jpg_.webp" alt="Product" className="w-full h-auto" />
           </div>
-          <div className="p-6 w-full md:w-1/2 bg-black p-2 rounded h-full w-full bg-black-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50">
+          <div className="p-6 w-full md:w-1/2 bg-black h-full bg-black-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50">
             {/* Product Description */}
             <h2 className="text-2xl md:text-6xl font-bold mb-4">Product Title</h2>
             <div>
-              <p className="text-lg mb-6">
+              <p className="text-md mb-6">
                 Profitez de délicieux barbecue toute l'année avec notre petit BBQ d'intérieur. Idéal pour les espaces restreints, cet appareil compact vous permet de préparer de savoureuses grillades dans le confort de votre maison.
               </p>            
               </div>
@@ -211,8 +225,72 @@ export default function Home() {
         </div>
       </div>
 
+      
 
-      <div className='max-w-screen-lg mx-auto mt-10 mb-5 flex p-2'>
+      <div className="">
+        <div className="">
+          <div className="bg-gray-100 shadow-lg overflow-hidden mx-auto w-full">
+            <div className="flex flex-col md:flex-row ">
+              {/* Product Description */}
+              <div className="p-6  bg-no-repeat bg-cover bg-center w-full md:w-1/2 text-center  text-white"
+              style={{backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)) , url(https://images.hdqwalls.com/wallpapers/fire-minimalist-1f.jpg)'}}
+              >
+
+     
+                <div className=" bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 w-full h-full">
+                  <h2 className="text-2xl md:text-3xl font-bold mb-10">Customer Reviews</h2>
+
+                  <div className="flex items-center justify-center ">
+                    <p className="text-4xl ml-2 mb-2">Do the job</p>
+                  </div>
+                  <div className="flex items-center justify-center text-4xl">
+                    <BsFillStarFill className="text-yellow-500 mr-1" />
+                    <BsFillStarFill className="text-yellow-500 mr-1" />
+                    <BsFillStarFill className="text-yellow-500 mr-1" />
+                    <BsFillStarFill className="text-yellow-500 mr-1" />
+                    <BsFillStarFill className="text-yellow-500" />
+                  </div>
+
+                  <div className="mt-10">
+                      Reseau
+                      <div className="w-12 h-12 flex items-center justify-center animate-pulse cursor-pointer">
+                        <div className="mx-auto">
+                          <FaTiktok className="w-full h-full text-white bg-clip-text bg-gradient-to-r from-pink-500 to-purple-500" />
+                        </div>
+                      </div>
+                    </div>
+
+                </div>
+
+
+                {/* Add more customer reviews here */}
+              </div>
+              
+
+              {/* Video */}
+              <div className="w-full md:w-1/2 bg-black">
+                <div className="aspect-w-16 aspect-h-9">
+                  <video className="w-full h-full object-cover" autoPlay loop muted>
+                    <source src="/video-product.mp4" type="video/mp4" />
+                  </video>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto flex flex-col md:flex-row items-center md:justify-between mt-3">
+          <p className="text-sm mb-2 md:mb-0">&copy; {new Date().getFullYear()} Your Website. All rights reserved.</p>
+          <Link href="/terms-and-conditions text-sm hover:underline">
+           Terms and Conditions
+          </Link>
+        </div>
+      </footer>
+
+      {/* <div className='max-w-screen-lg mx-auto mt-10 mb-5 flex p-2'>
         <div className='flex w-full text-4xl p-2 justify-around'>
 
       {
@@ -228,9 +306,9 @@ export default function Home() {
           </div>)
       }
         </div>
-      </div>
+      </div> */}
 
-
+{/* 
       <div className='max-w-screen-lg mx-auto mt-10 mb-5 flex justify-between p-2 ' >
         
           <div className="relative">
@@ -257,10 +335,10 @@ export default function Home() {
               </span>
             }
           </div>
-      </div>
+      </div> */}
 
 
-
+{/* 
       <div className='p-4 md:p-3'>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-screen-lg mx-auto">
@@ -300,54 +378,13 @@ export default function Home() {
 
         
       
-
-
-{/* 
-        <div className="flex items-center justify-center">
-        <div className="h-24 w-24 rounded-full bg-gray-200 relative">
-          <div className="absolute inset-0 rounded-full bg-gray-200 ring-4 ring-offset-4 ring-yellow-100 animate-pulse">
-            XD
-          </div>
-        </div>
-      </div> */}
-
-
-        {/* <div className="relative bg-transparent rounded-full">
-          <div className="absolute inset-0 bg-cover bg-center filter blur-md  bg-gradient-to-b from-cyan-500 to-green-500 rounded"></div>
-          <div className="absolute inset-0 bg-gray-200 bg-opacity-25 backdrop-filter backdrop-blur-lg rounded"></div>
-          <div className="absolute inset-0 flex items-center justify-center rounded">
-            <h1 className="text-3xl font-bold text-white">Content</h1>
-          </div>
-        </div> */}
-
-
-        {/* <div className="flex flex-col items-center justify-center py-8 border-b-[0.4em] border-emerald-500 rounded shadow-lg relative">
-        <div className="absolute inset-0 bg-cover bg-center filter blur-md bg-gradient-to-b from-cyan-500 to-green-500 rounded"></div>
-        <div className="absolute inset-0 bg-gray-200 bg-opacity-25 backdrop-filter backdrop-blur-lg rounded"></div>
-        <div className="w-full flex flex-col items-center justify-center rounded z-10">
-          <div className="text-3xl font-bold mb-2 text-center text-transparent bg-gradient-to-r from-cyan-500 to-emerald-500 bg-clip-text">OG Kush HHC</div>
-          <img src="https://legrossisteducbd.fr/wp-content/uploads/2022/12/Fleurs-CBD-Purple-Haze-Glass-House-2.png" alt="John Doe" className="w-40 h-40 object-cover rounded-full mb-2 hover:scale-110 transition duration-300 ease-in-out"/>
-          <div className="flex mt-5">
-              <button className="bg-gradient-to-r from-cyan-600 to-emerald-600 hover:from-emerald-500 hover:to-emerald-700 text-white font-bold py-2 px-4 rounded shadow-lg text-sm md:text-lg">
-                Ajouter au panier
-              </button>
-              <select className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm ml-2 shadow-lg">
-                <option>{exemplePrice.toLocaleString("fr-FR", { style: "currency", currency: "EUR" })}</option>
-              </select>
-          </div>
-        </div>
-        </div> */}
-
-
-
-        
-        
+   
 
 
       </div>
 
 
-    </div>
+      </div> */}
     </main>
   )
 }
